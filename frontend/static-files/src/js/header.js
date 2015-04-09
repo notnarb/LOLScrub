af93396c3a93e49878d661b89b8de3e4a85cc9af
Branton Horsley <brantonhorsley@notnarb.com>
2015-04-09 04:19:16 -0700

    Updated UI to a dark theme.  Added navbar.  User lookup errors now return 404s from the app server

--- frontend/static-files/src/js/header.js
+++ frontend/static-files/src/js/header.js
@@ -10,15 +10,19 @@ var loginTemplate = require('../tmpl/header/login.hbs');
 // The jquery header container
 var header;
 
-$('body').on('submit', '#header', login);
+$('body').on('submit', '#header', function (event) {
+	login();
+	// prevent actual submission of the form
+	return false;
+});
 
 $('body').on('click', '#header', function (event) {
 	var target = $(event.target);
 
 	var actionTarget = target.closest('[data-action]');
 
-	// If you click outside of an action, don't do anything
-	if (!actionTarget.length) {
+	// If you click outside of an action or you click a disabled action, don't do anything
+	if (!actionTarget.length || actionTarget.hasClass('disabled')) {
 		return;
 	};
 	var data = actionTarget.data();
@@ -52,13 +56,20 @@ module.exports.render = function () {
 function login () {
 	var input = header.find('[data-login]');
 	if (input && input.val()) {
-		userInfo.setName(input.val()).then(update)
+		var loginButton = header.find('[data-action="login"]');
+		loginButton.addClass('disabled');
+		var previousText = loginButton.html();
+		loginButton.html('Loading...');
+		userInfo.setName(input.val())
+			.then(update)
 			.catch(function (error) {
 				alert('Failed to log in');
+			})
+			.finally(function () {
+				loginButton.html(previousText);
+				loginButton.removeClass('disabled');
 			});
 	}
-	// submit handler
-	return false;
 }
 
 /**
--- frontend/static-files/src/js/main.js
+++ frontend/static-files/src/js/main.js
@@ -1,4 +1,6 @@
 "use strict";
+// Make it clear when I forget to require bluebird
+window.Promise = undefined;
 var Promise = require('bluebird');
 var containerTemplate = require('../tmpl/container.hbs');
 
@@ -13,9 +15,13 @@ $(function () {
 	var userInfo = require('./userInfo');
 	userInfo.init();
 
+	// One day I'll learn a proper framework....
 	var header = require('./header');
 	containerContent.find("#header").replaceWith(header.render());
 
+	var navbar = require('./navbar');
+	containerContent.find('#navbar').replaceWith(navbar.render());
+	
 	container.replaceWith(containerContent);
 });
 
