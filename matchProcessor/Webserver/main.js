f4699b3b65bce88c2103ef8db236528b8c3ece52
Ryan Summers <rsummers618@gmail.com>
2015-04-10 02:07:57 -0700

    Fixed bug with 1v1 Odds calculator Added webserver functionality for 1v1 Odds

--- matchProcessor/Webserver/main.js
+++ matchProcessor/Webserver/main.js
@@ -1,9 +1,10 @@
 var mongodb = require('mongodb');
 var express = require('express');
+var JSONStream = require('JSONStream');
 
 
 var db;
-var PORT = 3000;
+var PORT = 8000;
 var app = express();
 
 mongodb.connect ('mongodb://mongo:27017/urfday',function(err,db){
@@ -12,21 +13,18 @@ mongodb.connect ('mongodb://mongo:27017/urfday',function(err,db){
     if  (err){return console.dir(err);}
         console.log("We are connected to the DB");
 
-    app.configure(function () {
-        app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    /*app.configure(function () {
+        app.use(express.logger('dev'));     // 'default', 'short', 'tiny', 'dev'
         app.use(express.bodyParser());
-    });
+    });*/
 
 
     app.get('/SoloKillPercentageOdds/:ChampId/:EnemyId',function(req,res){
-        res.writeHead(200, {"Content-Type":"application/json"});
-        //db.collection('SoloKillPercentageOdds').find("KillerId":ChampId,"VictimId":EnemyId)[]);
-        res.end(JSON.stringify( ###### ));
-
-    }
-
-
-
+            res.writeHead(200, {"Content-Type":"application/json"});
+            console.log({KillerId:req.params.ChampId,VictimId:req.params.EnemyId});
+            db.collection('SoloKillPercentageOdds').find({KillerId:parseInt(req.params.ChampId),VictimId:parseInt(req.params.EnemyId)}).stream().pipe(JSONStream.stringify()).pipe(res);
+            console.log(db.collection('SoloKillPercentageOdds').find({KillerId:req.params.ChampId,VictimId:req.params.EnemyId})[0]);
+    });
 
 
 
